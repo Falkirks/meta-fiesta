abe16db051729e64df9a3d024ae4a50cb8396fd9
Falkirks <me@falkirks.com>
2016-04-30 14:51:27 -0700

    Added code and tests

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 'use strict';
-module.exports = function (str) {
-  console.log(str || 'Rainbow');
+var async = require('async');
+var SandCastle = require('sandcastle').SandCastle;
+var sandcastle = new SandCastle();
+var execute = function(obj, test, cb){
+  var script = sandcastle.createScript(obj.logic);
+  script.on('exit', function(err, output) {
+    cb(null, output === true);
+  });
+  script.on('timeout', function() {
+    cb(null, false);
+  });
+  script.run({test: test});
+};
+module.exports = function (objects, test, cb) {
+  for(var i = 0; i < objects.length; i++) {
+    if (!objects[i].logic){
+      return cb(new Error('"objects" are all required to have a "logic" property, failed at ' + i + '.'), null);
+    }
+  }
+  async.filter(objects, function(obj, cb){
+    execute(obj, test, cb);
+  }, function(err, results){
+    cb(null, results);
+  });
 };
--- test/test.js
+++ test/test.js
@@ -3,8 +3,40 @@ var assert = require('assert');
 var metaFiesta = require('../');
 
 describe('meta-fiesta node module', function () {
-  it('must have at least one test', function () {
-    metaFiesta();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+  this.timeout(6000);
+  it('must correctly filter results', function (done) {
+    var objects = [
+      { logic: "exports.main = function() { exit(test === 2);}", otherStuff: "yo" },
+      { logic: "exports.main = function() {exit(test === 1);}", otherStuff: "yoyo" }
+    ];
+    var test = 1;
+
+    metaFiesta(objects, test, function(err, list){
+      assert("yoyo", list[0].otherStuff);
+      assert(list.length, 1);
+      done();
+    });
+  });
+  it('must time out functions', function (done) {
+    var objects = [
+      { logic: "exports.main = function() { while(true){}}"}
+    ];
+    var test = 1;
+
+    metaFiesta(objects, test, function(){
+      done();
+    });
+  });
+  it('must error when logic is not found', function (done) {
+    var objects = [
+      { logic: "exports.main = function() {}"},
+      { noLogicHere: "foo"}
+    ];
+    var test = 1;
+
+    metaFiesta(objects, test, function(err){
+      assert(err !== null, true);
+      done();
+    });
   });
 });
